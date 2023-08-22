3be75b2f051b1583c0821a21109537eef49462c2
mrwolf <ya.aw2010@gmail.com>
2023-08-22 18:04:55 +0400

    save & get for one string

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"strings"
 )
+
+var mytext = "empty"
 
 func main() {
 
 	ctx := context.Background()
-
 	opts := []bot.Option{
 		bot.WithDefaultHandler(handler),
 	}
@@ -24,16 +25,19 @@ func main() {
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	if update.Message.Text == "/start" {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		//Text: update.Message.Text,
+	})
+
+	switch {
+	case strings.HasPrefix(update.Message.Text, "save"):
+		mytext = strings.TrimPrefix(update.Message.Text, "save")
+	case strings.HasPrefix(update.Message.Text, "get"):
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
-			Text:   "Greetings! I'm a little wolf bot, nice to meet you!I have know two command:\n\n/save + you'r message - I will remember it\n/get - I will write the message, which I have remember",
-		})
-	} else {
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   update.Message.Text,
+			Text:   mytext,
 		})
 	}
-	fmt.Println(update.Message.Text)
+
 }
